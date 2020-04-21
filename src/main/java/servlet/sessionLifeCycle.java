8bc06b0dcc4396975f2a97ecb8753db6204e1e98
MMN165 <55259986+MMN165@users.noreply.github.com>
2020-04-21 17:52:18 -0400

    Update sessionLifeCycle.java

--- src/main/java/servlet/sessionLifeCycle.java
+++ src/main/java/servlet/sessionLifeCycle.java
@@ -44,7 +44,7 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
 
       // Create a link so the user can create a new session.
       // The link will have a parameter builtin
-      String lifeCycleURL = "/servlet/sessionLifeCycle";
+      String lifeCycleURL = "/servlet";
       out.println("<a href=\"" + lifeCycleURL + "?action=newSession\">");
       out.println("Create new session</A>");
 
@@ -92,7 +92,7 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
       out.print  ("<br>Maximum inactive interval (seconds): ");
       out.println(session.getMaxInactiveInterval());
 
-      String lifeCycleURL = "/servlet/sessionLifeCycle";
+      String lifeCycleURL = "/servlet";
       out.print  ("<br><br><a href=\"" + lifeCycleURL + "?action=invalidate\">");
       out.println("Invalidate the session</a>");
       out.print  ("<br><a href=\"" + lifeCycleURL + "\">");
